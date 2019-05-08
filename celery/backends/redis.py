649c90f7dba0b1f2b9feb34cb7b36473321b2b0a
Dominic LoBue <dominic.lobue@gmail.com>
2019-05-07 17:04:30 -0700

    Remove AsyncBackendMixin from RedisBackend
    We do not want or need async support in the redisbackend, and it is just
    causing problems. Remove the mixin and any related methods.

--- celery/backends/redis.py
+++ celery/backends/redis.py
@@ -21,7 +21,7 @@ from celery.utils.log import get_logger
 from celery.utils.time import humanize_seconds
 
 from .asynchronous import AsyncBackendMixin, BaseResultConsumer
-from .base import BaseKeyValueStoreBackend
+from .base import KeyValueStoreBackend
 
 try:
     from urllib.parse import unquote
@@ -148,10 +148,8 @@ class ResultConsumer(BaseResultConsumer):
             self._pubsub.unsubscribe(key)
 
 
-class RedisBackend(BaseKeyValueStoreBackend, AsyncBackendMixin):
+class RedisBackend(KeyValueStoreBackend):
     """Redis task result store."""
-
-    ResultConsumer = ResultConsumer
 
     #: :pypi:`redis` client module.
     redis = redis
@@ -229,10 +227,6 @@ class RedisBackend(BaseKeyValueStoreBackend, AsyncBackendMixin):
         self.connection_errors, self.channel_errors = (
             get_redis_error_classes() if get_redis_error_classes
             else ((), ()))
-        self.result_consumer = self.ResultConsumer(
-            self, self.app, self.accept,
-            self._pending_results, self._pending_messages,
-        )
 
     def _params_from_url(self, url, defaults):
         scheme, host, port, _, password, path, query = _parse_url(url)
@@ -289,10 +283,6 @@ class RedisBackend(BaseKeyValueStoreBackend, AsyncBackendMixin):
         connparams.update(query)
         return connparams
 
-    def on_task_call(self, producer, task_id):
-        if not task_join_will_block():
-            self.result_consumer.consume_from(task_id)
-
     def get(self, key):
         return self.client.get(key)
 
@@ -325,10 +315,6 @@ class RedisBackend(BaseKeyValueStoreBackend, AsyncBackendMixin):
                 pipe.set(key, value)
             pipe.publish(key, value)
             pipe.execute()
-
-    def forget(self, task_id):
-        super(RedisBackend, self).forget(task_id)
-        self.result_consumer.cancel_for(task_id)
 
     def delete(self, key):
         self.client.delete(key)
